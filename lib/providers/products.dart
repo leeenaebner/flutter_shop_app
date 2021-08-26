383cb589d053e790b66f7abbba21a368f646f86f
Lena Ebner <lena.ebner@mway.io>
2021-08-26 17:14:21 +0200

    update notes

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'product.dart';
 
 class Products with ChangeNotifier {
@@ -47,13 +48,29 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
-    _items.add(Product(
-      id: DateTime.now().toString(),
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-    ));
+    final url =
+        "https://flutter-shop-app-91947-default-rtdb.europe-west1.firebasedatabase.app/products.json";
+    http
+        .post(
+          Uri.parse(url),
+          body: json.encode({
+            'title': product.title,
+            'description': product.description,
+            'imageUrl': product.imageUrl,
+            'price': product.price,
+            'isFavorite': product.isFavorite,
+          }),
+        )
+        .then((response) => {
+              _items.add(Product(
+                id: json.decode(response.body)['name'],
+                title: product.title,
+                description: product.description,
+                price: product.price,
+                imageUrl: product.imageUrl,
+              ))
+            });
+
     notifyListeners();
   }
 
